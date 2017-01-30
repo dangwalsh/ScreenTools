0b466c0903a3a1e894db7705d7712fd70db0657c
Daniel Walsh <dangwalsh@gmail.com>
2017-01-30 17:02:35 -0500

    updated unit tests

---
+++ ScreenTools.Tests/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace ScreenTools.Tests
+{
+    public class UnitTest
+    {
+        const string screenfile = @"c:\users\22791\desktop\screen_test.png";
+        const string windowfile = @"c:\users\22791\desktop\window_test.png";
+
+        [Fact]
+        public void GetScreenImage()
+        {
+            Capture.ScreenToFile(screenfile);
+            bool exists = System.IO.File.Exists(screenfile);
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public void GetWindowImage()
+        {
+            Capture.ActiveWindowToFile(windowfile);
+            bool exists = System.IO.File.Exists(windowfile);
+            Assert.True(exists);
+        }
+    }
+}
